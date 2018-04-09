612dd8e753b4544c0e226764d073da85deb0b7ae
Ralf-Eric Winter <ralf-eric.winter@xitaso.com>
2018-04-09 13:04:13 +0200

    session service prototype

--- src/app/services/session/session.service.ts
+++ src/app/services/session/session.service.ts
@@ -8,4 +8,16 @@ export class SessionService {
 
   constructor(private http: HttpClient) { }
 
+  /** Starts a session and returned the new id */
+  public startSession(): Promise<number> {
+    const url = this.serverBase + 'StartSession';
+    return this.http.post<number>(url, null).toPromise();
+  }
+
+  /** Stop a session */
+  public stopSession(): Promise<void> {
+    const url = this.serverBase + 'StopSession';
+    return this.http.post<void>(url, null).toPromise();
+  }
+
 }
